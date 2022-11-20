df3f2124ecfbe9e4ec73c5fdde3af04ca5632117
nazaropr <90557490+nazaropr@users.noreply.github.com>
2022-11-20 20:09:30 +0200

    first lab
    Co-Authored-By: nazaropr <90557490+nazaropr@users.noreply.github.com>

---
+++ lab1/AnalizerClassLibraryTests/CalcClassTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalcClassBr;
+using System;
+using ErrorLibrary;
+
+namespace AnalaizerClassLibrary.Tests
+{
+    [TestClass]
+    public class CalcClassTests
+    {
+        public TestContext TestContext { get; set; }
+     
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", "testData.xml", "TestWithValidNumbers", DataAccessMethod.Sequential)]
+        public void Mod_WhenNumbersAreValid_ReturnsExpectedResult()
+        {
+            //Arrange
+            long incomingFirstNumber = Convert.ToInt64(TestContext.DataRow["incomingFirstNumber"]);
+            long incomingSecondNumber = Convert.ToInt64(TestContext.DataRow["incomingSecondNumber"]);
+            long expected = Convert.ToInt64(TestContext.DataRow["expectedResult"]);
+
+            //Actual
+            long actual = CalcClass.Mod(incomingFirstNumber, incomingSecondNumber);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", "testData.xml", "TestWithInvalidNumbers", DataAccessMethod.Sequential)]
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException), ErrorsExpression.ERROR_06)]
+        public void Mod_WhenNumbersAreInvalid_ThrowsArgumentOutOfRangeException()
+        {
+            //Arrange
+            long incomingFirstNumber = Convert.ToInt64(TestContext.DataRow["incomingFirstNumber"]);
+            long incomingSecondNumber = Convert.ToInt64(TestContext.DataRow["incomingSecondNumber"]);
+
+            //Actual
+            long actual = CalcClass.Mod(incomingFirstNumber, incomingSecondNumber);
+
+            //Assert - Expects exception ArgumentOutOfRangeException
+        }
+
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", "testData.xml", "TestWithZeroDivider", DataAccessMethod.Sequential)]
+        [TestMethod]
+        [ExpectedException(typeof(System.DivideByZeroException), ErrorsExpression.ERROR_09)]
+        public void Mod_WhenSecondNumberIsZero_ThrowsDivideByZeroException()
+        {
+            //Arrange
+            long incomingFirstNumber = Convert.ToInt64(TestContext.DataRow["incomingFirstNumber"]);
+            long incomingSecondNumber = Convert.ToInt64(TestContext.DataRow["incomingSecondNumber"]);
+
+            //Actual
+            long actual = CalcClass.Mod(incomingFirstNumber, incomingSecondNumber);
+
+            //Assert - Expects exception ArgumentOutOfRangeException
+        }
+    }
+}
